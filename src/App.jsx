ce21b9c49da610c5aad072381af57cd5a5754798
Alexy Araujo <96698163+a-gva@users.noreply.github.com>
2022-02-18 20:55:46 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import CounterTwo from './components/CounterTwo'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <CounterTwo />
+    </div>
+  )
+}
+
+export default App
